3bde6dff1646805ec11b030f789c1e38713c6dbe
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 13:46:20 -0500

    set up material ui themes

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -43,3 +43,64 @@ export const tokens = {
       main: "#1f2026",
     },
 };
+
+// mui theme settings
+export const themeSettings = {
+    palette: {
+      primary: {
+        ...tokens.primary,
+        main: tokens.primary[500],
+        light: tokens.primary[400],
+      },
+      secondary: {
+        ...tokens.secondary,
+        main: tokens.secondary[500],
+      },
+      tertiary: {
+        ...tokens.tertiary,
+      },
+      grey: {
+        ...tokens.grey,
+        main: tokens.grey[500],
+      },
+      background: {
+        default: tokens.background.main,
+        light: tokens.background.light,
+      },
+    },
+    typography: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h2: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h3: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 20,
+        fontWeight: 800,
+        color: tokens.grey[200],
+      },
+      h4: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 14,
+        fontWeight: 600,
+        color: tokens.grey[300],
+      },
+      h5: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 12,
+        fontWeight: 400,
+        color: tokens.grey[500],
+      },
+      h6: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 10,
+        color: tokens.grey[700],
+      },
+    },
+};
