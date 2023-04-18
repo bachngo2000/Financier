f9f5a2e3017a9391c236c1f0228d371f55723f89
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 16:19:50 -0500

    implemented reponsive design using grid templates

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,9 +1,9 @@
-import { useTheme } from "@mui/material";
+import { useMediaQuery, useTheme } from "@mui/material";
 import { Box } from '@mui/material';
 
 type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -14,16 +14,56 @@ const gridTemplate = `
     "g h i"
     "g h j"
     "g h j"
-`
+`;
+
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+`;
+
 
 const Dashboard = (props: Props) => {
+const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
     const { palette } = useTheme();
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
-        sx={{
+        sx={
+            isAboveMediumScreens ? {
             gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
             gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-            gridTemplateAreas: gridTemplate,
+            gridTemplateAreas: gridTemplateLargeScreens,
+        } : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,
         }}>
         
         <Box bgcolor="#fff" gridArea="a"></Box>
