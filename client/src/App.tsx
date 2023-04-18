2c7d95abbba3b3b25bb36921dee1669320c1cf02
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 14:00:27 -0500

    set up material ui for use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {createTheme} from "@mui/material/styles";
+import {useMemo} from "react";
+import { themeSettings } from "./theme";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
   return (
     <div className="app">
-     
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   )
 }
