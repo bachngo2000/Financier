96454bf8f5855f1eea0d4c761412e0e6d406c535
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 18:41:46 -0500

    creating API call and ensuring backend and frontend are connected

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
+
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,6 +23,10 @@ app.use(cors());
 
 console.log("Hello");
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -29,10 +37,9 @@ mongoose
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+
   })
   .catch((error) => console.log(`${error} did not connect`))
