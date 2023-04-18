36d5b91fe1ca2f4f5b6521725cc3117673384882
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 16:36:18 -0500

    set up dashboard layout

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,8 +1,9 @@
-import DashboardBox from "@/components/DashboardBox";
-import { useMediaQuery, useTheme } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import { Box } from '@mui/material';
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-type Props = {}
 
 const gridTemplateLargeScreens = `
     "a b c"
@@ -51,9 +52,9 @@ const gridTemplateSmallScreens = `
 `;
 
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
 const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
-    const { palette } = useTheme();
+
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
         sx={
@@ -66,17 +67,10 @@ const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
             gridAutoRows: "80px",
             gridTemplateAreas: gridTemplateSmallScreens,
         }}>
-        
-        <DashboardBox  gridArea="a"></DashboardBox>
-        <DashboardBox  gridArea="b"></DashboardBox>
-        <DashboardBox  gridArea="c"></DashboardBox>
-        <DashboardBox  gridArea="d"></DashboardBox>
-        <DashboardBox  gridArea="e"></DashboardBox>
-        <DashboardBox  gridArea="f"></DashboardBox>
-        <DashboardBox  gridArea="g"></DashboardBox>
-        <DashboardBox  gridArea="h"></DashboardBox>
-        <DashboardBox  gridArea="i"></DashboardBox>
-        <DashboardBox  gridArea="j"></DashboardBox>
+
+        <Row1></Row1>
+        <Row2></Row2>
+        <Row3></Row3>
 
     </Box>
   );
