9cea508193874e7c228e14be573650e2ab044277
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-18 22:00:11 -0500

    finished row 2 charts

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -9,13 +9,21 @@ import {
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
       getKpis: build.query<Array<GetKpisResponse>, void>({
         query: () => "kpi/kpis/",
         providesTags: ["Kpis"],
       }),
+      getProducts: build.query<Array<GetProductsResponse>, void>({
+        query: () => "product/products/",
+        providesTags: ["Products"],
+      }),
+      getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+        query: () => "transaction/transactions/",
+        providesTags: ["Transactions"],
     }),
+  }),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery,  useGetProductsQuery, useGetTransactionsQuery } = api;
--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js";
+
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import { kpis, products } from "./data/data.js";
 
 
 /* CONFIGURATIONS */
@@ -25,6 +28,8 @@ console.log("Hello");
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+
 
 
 /* MONGOOSE SETUP */
@@ -40,6 +45,7 @@ mongoose
     // /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
+    // Product.insertMany(products);
 
   })
   .catch((error) => console.log(`${error} did not connect`))
